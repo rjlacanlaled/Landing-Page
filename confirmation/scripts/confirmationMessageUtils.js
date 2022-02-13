fc8eca83aeb3c1e473e025d36a340d85be003404
rjlacanlaled <rjlacanlaled@gmail.com>
2022-02-14 02:26:04 +0800

    major update

--- confirmation/scripts/confirmationMessageUtils.js
+++ confirmation/scripts/confirmationMessageUtils.js
@@ -4,10 +4,12 @@ const confirmationMessageContainer =
 function createConfirmationPageHTMLElements(userDetails) {
   if (userDetails == null) return;
 
-  const thankYouMessage = `Thank you ${userDetails.firstName} ${userDetails.lastName} for subscribing!`;
-  const planDetails = `Your plan is ${userDetails.plan}.`;
-  const freebieDetails = `You chose ${userDetails.freeAlbum} as your freebie. This CD will be sent to your address within 3 business days.`;
-  const emailReceipt = `Complete details will be sent to ${userDetails.email} within 5 minutes.`;
+  const userDetailsParams = new URLSearchParams(userDetails);
+
+  const thankYouMessage = `Thank you ${userDetailsParams.get('firstName')} ${userDetailsParams.get('lastName')} for subscribing!`;
+  const planDetails = `Your plan is ${userDetailsParams.get('subscriptionPlans')}.`;
+  const freebieDetails = `You chose ${userDetailsParams.get('freeAlbums')} as your freebie. This CD will be sent to your address within 3 business days.`;
+  const emailReceipt = `Complete details will be sent to ${userDetailsParams.get('email')} within 5 minutes.`;
   const supportMessage = `If you have any questions, you can contact our support team at support@motownstreaming.com`;
 
   confirmationMessageContainer.appendChild(createElementWithOptions("h1", {text: thankYouMessage}));
--- scripts/form.js
+++ scripts/form.js
@@ -1,11 +1,60 @@
 const SUBSCRIPTION_FORM_ID = "subscriptionForm";
 const targetUrl = "https://landing-page-confirmation.vercel.app/";
 
+const subscriptionPlanSelection = document.getElementById('subscriptionPlans');
+const freeAlbumSelection = document.getElementById('freeAlbums');
+
 document.forms[SUBSCRIPTION_FORM_ID].onsubmit = onFormSubmit;
+
+(() => {
+    addSubscriptionPlanOptions();
+    addFreeAlbumOptions();
+})();
+
+// Form Data Helpers
+
+function addSubscriptionPlanOptions() {
+    const planOptions = planItemParams.map(item => {
+        return new Option(item.planTitleText, item.planTitleText);
+    });
+
+    planOptions.forEach(option => {
+        subscriptionPlanSelection.add(option, undefined);
+    });
+
+}
+
+function addFreeAlbumOptions() {
+    const freeAlbumOptions = freebieItemParams.map(item => {
+        return new Option(item.mainText, item.mainText);
+    });
+
+    freeAlbumOptions.forEach(option => {
+        freeAlbumSelection.add(option, undefined);
+    });
+}
+
+function getInputDataUrl() {
+    const inputElements = Array.apply(null, document.querySelectorAll('[data-input]'));
+    let inputArr = '';
+    inputElements.forEach((element, index) => {
+        if (element.value === null) return
+        inputArr += `${element.id}=${element.value}`;
+        if (index === inputElements.length - 1) return;
+        inputArr += '&';
+    });
+    return inputArr;
+}
+
+// End Form Data Helpers
+
+
+// Event handlers
 
 function onFormSubmit(e) {
     e.preventDefault();
-    const currentUrl = window.location.search;
-    const currentUrlParams = decodeURIComponent(getSearchParams(currentUrl));
-    window.location.replace(`${targetUrl}?${currentUrlParams}`);
+    console.log(getInputDataUrl());
+    //window.location = `${targetUrl}?${getInputDataUrl()}`;
 }
+
+// Event handlers
